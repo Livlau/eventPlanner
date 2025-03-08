a9be6a56f9f016a41991b89c4f2fe46062c5354a
Livlau <kodekode2021@gmail.com>
2025-03-08 12:29:39 -0500

    update eventplanner and footer components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EventPlanner.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2025 by Event Planner Organization. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
